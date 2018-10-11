eee2a6acdbee7920bb339d479ca4f54d753ef5ae
Unknown <teaguefcs@gmail.com>
2018-10-11 00:33:08 -0400

    Added Gun, Full level, and light

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour {
 
     public float moveSpeed;
     public GameObject bullet;
+    public GameObject gun;
 
 	// Use this for initialization
 	void Start () {
@@ -20,17 +21,21 @@ public class Player : MonoBehaviour {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
+
         transform.Rotate(0, mouseX*4, 0);
-        Camera.main.transform.Rotate(-mouseY*4, 0, 0);
+        print(Camera.main.transform.rotation.eulerAngles.x);
+        if (Mathf.Abs(-mouseY*4+Camera.main.transform.rotation.eulerAngles.x) < 60 || Mathf.Abs(-mouseY * 4 + Camera.main.transform.rotation.eulerAngles.x) > 300) {
+            Camera.main.transform.Rotate(-mouseY * 4, 0, 0);
+        }
 
         inputVector = transform.forward * Input.GetAxis("Vertical");
         inputVector += transform.right * Input.GetAxis("Horizontal");
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            GameObject b = Instantiate(bullet, Camera.main.transform.position + (transform.forward*2), Camera.main.transform.rotation);
-            b.transform.Rotate(-10, 0, 0);
-            b.GetComponent<Rigidbody>().AddForce(b.transform.forward*10, ForceMode.Impulse);
+            GameObject b = Instantiate(bullet, gun.transform.position + (gun.transform.up*.25f), gun.transform.rotation);
+            b.transform.Rotate(-5, 0, 0);
+            b.GetComponent<Rigidbody>().AddForce(b.transform.forward * .25f, ForceMode.Impulse);
         }
 	}
 
